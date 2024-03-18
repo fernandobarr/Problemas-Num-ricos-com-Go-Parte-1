79e43dc26b034ae893c4d2b3588adc0ee6c1d781
Fernando Justino de Barros <fernandobarr@gmail.com>
2024-03-18 12:37:34 -0300

    Ajuste no Nome da varievel

--- Problemas1.go
+++ Problemas1.go
@@ -5,11 +5,11 @@ import "fmt"
 func prob_Num() {
 	for i := 1; i <= 100; i++ {
 		var divisivel int
-		var pim int = 3
-		divisivel = i % pim
+		var pin int = 3
+		divisivel = i % pin
 		if divisivel == 0 {
 			fmt.Println(i)
-			//fmt.Printf("Pim - %v e %v = %v ", i, pim, divisivel)
+			//fmt.Printf("Pin - %v e %v = %v ", i, pin, divisivel)
 			//fmt.Println("")
 		}
 		//else {
